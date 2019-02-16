ddb5fb58fe36f44ecba6649aa33f7d8b52ac3573
shgysd <shgysd@gmail.com>
2019-02-17 03:37:59 +0900

    Add react-router

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+});
 
 module.exports = app;
